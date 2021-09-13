e4cf13172d0bff0fbe7fa94936447e752bedd0b1
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-13 13:00:23 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
