97fbe0fa691d8058e8487bbb106d5c6fc09d4108
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-13 13:22:16 +0900

    buttons and linked the draw API

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let deckId 
+
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
@@ -5,3 +7,10 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+    `) 
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
