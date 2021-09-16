7bff5d3263ab05018c1a99231e70154128794986
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-16 14:22:42 +0900

    working. just need to centre

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ let deckId
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
 
 document.getElementById("draw-cards").addEventListener("click", () => {
-    fetch(`https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
-    `) 
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=2
+    `)
         .then(res => res.json())
-        .then(data => console.log(data))
-        document.getElementById("cards").innerHTML = `
-        <img src=${data.cards[0].image} />
-        <img src=${data.cards[1].image} />
-        `
-    })
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            document.getElementById("cards").children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
+        })
+})
