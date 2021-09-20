ecc9021f64f3a99c42439e8c1b2ae26a8dd9453b
K Sheenan <kaynesheenan+git@gmail.com>
2021-09-20 23:47:30 +0900

    display remaining cards when New Deck button pressed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
         })
 }
@@ -19,14 +20,13 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            // remainingText.textContent = `Remaining cards: ${data.remaining}`
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-
             const winnerText = determineCardWinner(data.cards[0], data.cards[1])
             header.textContent = winnerText
         })
@@ -37,8 +37,6 @@ function determineCardWinner(card1, card2) {
     "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
-    console.log("card 1:", card1ValueIndex)
-    console.log("card 2:", card2ValueIndex)
     
     if (card1ValueIndex > card2ValueIndex) {
         return "Card 1 wins!"
@@ -48,12 +46,3 @@ function determineCardWinner(card1, card2) {
         return "War!"
     }
 }
-
-const card1Obj = {
-    value: "7"
-}
-const card2Obj = {
-    value: "3"
-}
-
-determineCardWinner(card1Obj, card2Obj)
